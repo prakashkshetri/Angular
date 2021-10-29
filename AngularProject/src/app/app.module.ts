924119fcec915e7008a89a56c997cdf14a921fdf
Adhikari Prakash <31641648+prakashkshetri@users.noreply.github.com>
2021-10-29 15:30:09 +0900

    project files

---
+++ AngularProject/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SigninComponent } from './components/signin/signin.component';
+
+import { MainComponent } from './components/main/main.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    NavbarComponent,
+    SigninComponent,
+    MainComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AngularProject/src/app/models/User.ts
@@ -0,0 +1,8 @@
+export interface User{
+firstName: string,
+lastName: string,
+isActive?: boolean,
+email: string,
+registered?: any,
+hide?: boolean
+}
